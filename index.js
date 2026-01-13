848a10d4f3e7064690e639a519fe9c44e3418732
namanydv <namanofficial91@gmail.com>
2026-01-13 22:27:43 +0530

    Routing in Express JS

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import http, { request } from "http"
 
-const server = http.createServer((request,Response)=>{
-    Response.end("Welcome to my server")
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+    if(req.url =="/"){
+        res.end("Welcome to home Route")
+    }
+    else if(req.url == "/about"){
+        res.end("Welcome to About Route")
+    }
+    else if(req.url == "/contact"){
+        res.end("Welcome to Contact Page ")
+    }
+    else{
+        res.end("404 Not Found")
+    }
 })
 
-server.listen(4000,()=>{
+server.listen(port,()=>{
     console.log("Server is Started")
 })
+
+// const server = http.createServer((request,Response)=>{
+//     Response.end("Welcome to my server")
+// })
+
+// server.listen(4000,()=>{
+//     console.log("Server is Started")
+// })
 
