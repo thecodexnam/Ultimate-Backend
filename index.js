2028cd14e3049de3204afbb4e6b62e34eb2d846b
namanydv <namanofficial91@gmail.com>
2026-01-14 21:57:29 +0530

    .

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-import http, { request } from "http"
+import express from "express"
+const server = express()
 
-const port = 3000;
+const port = 4000;
 
-const server = http.createServer((req,res)=>{
-    if(req.url =="/"){
-        res.end("Welcome to home Route")
-    }
-    else if(req.url == "/about"){
-        res.end("Welcome to About Route")
-    }
-    else if(req.url == "/contact"){
-        res.end("Welcome to Contact Page ")
-    }
-    else{
-        res.end("404 Not Found")
-    }
+server.get("/code",(req,res)=>{
+    res.send("here is our new server")
 })
 
-server.listen(port,()=>{
-    console.log("Server is Started")
+server.listen(4000,()=>{
+    console.log("Our new Server is Started")
 })
+
+
+// const app = express()
+
+// const port = 4000;
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello this is home page")
+// })
+// app.get("/about",(req,res)=>{
+//     res.send("Hello this is About")
+// })
+// app.get("/contact",(req,res)=>{
+//     res.send("Hello Contact")
+// })
+
+// app.listen(port,()=>{
+//     console.log("Server is Started at port no ",port)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import http, { request } from "http"
+
+// const port = 3000;
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url =="/"){
+//         res.end("Welcome to home Route")
+//     }
+//     else if(req.url == "/about"){
+//         res.end("Welcome to About Route")
+//     }
+//     else if(req.url == "/contact"){
+//         res.end("Welcome to Contact Page ")
+//     }
+//     else{
+//         res.end("404 Not Found")
+//     }
+// })
+
+// server.listen(port,()=>{
+//     console.log("Server is Started")
+// })
 
 
