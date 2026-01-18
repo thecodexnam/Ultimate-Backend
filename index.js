14fc2cd5947598541e65da070b04ad59a6002c7b
namanydv <namanofficial91@gmail.com>
2026-01-18 21:11:53 +0530

    params and Query in backend

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
+import express from "express"
+const server = express()
+
+const port = 4000;
+
+const users = [
+    { name: "Naman",
+      AGE : 20,  
+     },
+    { name: "rohan" },
+    { name: "Karan" },
+]
+
+server.get("/", (req, res) => {
+    res.send("This is the home page of your 4000 server port")
+})
+
+server.get("/user", (req, res) => {
+    const name = req.query.name
+
+    if (!name) {
+        return res.send("Please provide name in query")
+    }
+
+    const user = users.find(
+        u => u.name.toLowerCase() === name.toLowerCase()
+    )
+
+    if (!user) {
+        return res.send("User not found")
+    }
+
+    res.json(user)
+})
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+
+
+
+
+//server is created by express js
+
+// import express from "express"
+// const server = express()
+
+// const port = 4000;
+
+// server.get("/",(req,res)=>{
+//     res.send("this is the home page")
+// })
+
+
+// server.listen(port)
+
+
+
+// this server is created by node js
+// import http from "http"
+
+// const port = 4000;
+
+// const server = http.createServer((req,res)=>{
+//     console.log("your server is started")
+//     if(req.url=="/"){
+//         res.end("this is the home page of your server")
+//     }
+//     else if(req.url == "/about"){
+//          res.end("welcome to the about page")
+//     }
+// })
+
+// server.listen(port)
+
+
+
+
 // import express from "express"
 // const app = express()
 
