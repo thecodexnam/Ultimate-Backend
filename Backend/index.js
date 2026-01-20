6e45c626c78051e555f65fca6745c6656d9295e1
namanydv <namanofficial91@gmail.com>
2026-01-20 21:23:06 +0530

    Implement POST request handling and update frontend to use Axios for data submission

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,25 @@
 import express from "express"
-import cors from "cors"
+import cors from "cors" // cross origin resourse 
 
 const server = express()
 
-server.use(cors())
+server.use(cors({
+    origin:"http://localhost:5173"
+}))
+
+server.use(express.json())
+
 const port = 5000;
 
 server.get("/",(req,res)=>{
     res.json({name:"Naman",age:20,course:"BCA"})
 })
+
+server.post("/",(req,res)=>{
+    console.log(req.body);    
+    res.send({success:true})
+})
+
 
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 const App = () => {
+  let[username,setUsername] = useState('')
+  let[city,setCity] = useState('')
+  let[age,setAge] = useState('')
 
   async function getRes() {
-    const res = await fetch("http://localhost:4000/")
-    let data = await res.json();
-    console.log(`This is the value of ${data}`)
+    // try {
+    //   const res = await fetch("http://localhost:5000/")
+    //   const data = await res.json()
+    //   console.log(data)
+    // } catch (error) {
+    //   console.log(error)
+    // }
+
+  axios.post("http://localhost:5000/",
+    {username,
+     city,
+     age
+    })
+  .then((e)=>{
+    console.log(e.data.name);
+  })
+
+  .catch((error)=>{
+    console.log(error);
+  })
   }
+
+
 
   return (
     <div>
-      <button onClick={()=>getRes()}>Send</button>
+      {/* <button onClick={getRes}>Send</button> */}
+      <input type="text" placeholder='UserName' value={username} onChange={(e) =>setUsername(e.target.value)} />
+      <input type="text" value={city} placeholder='City' onChange={(e) =>setCity(e.target.value)}  />
+      <input type="text" value={age} placeholder='Enter Your age' onChange={(e) =>setAge(e.target.value)} />
+      <button onClick={()=>getRes()}>Send Data</button>
     </div>
   )
 }
