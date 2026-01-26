a852c67fa2f7542c5a70deb7fedc1f80640095ca
namanydv <namanofficial91@gmail.com>
2026-01-26 07:47:35 +0530

    Revised express,node and http methods

--- Backend/index.js
+++ Backend/index.js
@@ -1,47 +1,117 @@
-import express from "express"
-import cors from "cors"
+import express from 'express'
+import { use } from 'react';
 
-const server = express()
-const port = 4444;
+const port = 4444
 
-server.use(cors()) //It allow frontend Request. CORS is a security feature that controls which frontends are allowed to access a backend.
+const server = express();
 
-server.use(express.json())// cliend Sends data is JSON , Server Doesn't understand JSON by default. express.json() is like a translator that convert JSON Data into JS Object
+const users = [
+    {id:1,
+     name:"Naman",
+     role:"Developer"
+    },
+    {id:2,
+     name:"Rohan",
+     role:"UI/UX developer"
+    },
+    {id:3,
+     name:"CodeXnam",
+     role:"Backend Developer"
+    },
+    { id:4,
+    name:"Something",
+    role:"Tester"
 
-const password = "Naman1234"
-
-
-server.get("/",(req,res)=>{
-    res.send("<h1>This is our Home Page and also a Main Page </h1>")
-});
-
-//Post API to received Data from frontend
-server.post("/",(req,res)=>{
-    console.log("Received Data",req.body);
-    const{username,city,age,pass} = req.body;
-
-    //PassWord Check = if it is incorrect
-    if(pass !== password){
-        return res.status(401).json({message: "Password does not Match"})
     }
 
-    //send Response back to frontend
-    res.json({
-        message:"Data Received Successfully",
-        name : username,
-        city,
-        age,
-    });
-});
+]
+
+server.get("/",(req,res)=>{
+    res.send("This is over server that created by express JS")
+})
+
+server.get("/user",(req,res)=>{
+    const{name,age} = req.query
+    res.send(`The name of user is ${name} and the age is ${age}`)
+})
+
+server.get("/access/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    console.log(`the id is ${id}`)
+    const user = users.find(user => user.id === id)
+
+    if(!user){
+        res.status(400).send(`Invalid Id`)
+    }
+    else{
+        res.json(user)
+    }
+})
 
 server.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+    console.log(`this server run on port number ${port}`)
 })
 
 
 
+// import express from "express"
+// import cors from "cors"
+
+// const server = express()
+// const port = 4444;
+
+// server.use(cors())//It allow frontend Request. CORS is a security feature that controls which frontends are allowed to access a backend.
+
+// server.use(express.json())// cliend Sends data is JSON , Server Doesn't understand JSON by default. express.json() is like a translator that convert JSON Data into JS Object
+
+// const password = "Naman1234"
 
 
+// server.get("/",(req,res)=>{
+//     res.send("<h1>This is our Home Page and also a Main Page </h1>")
+// });
+
+// server.get("/user", (req, res) => {
+//   const response = {
+//     name: "Naman",
+//     role: "Developer",
+//     message:"This response is come from Backend Side"
+//   };
+
+//   res.json(response); // send data to client
+// });
+
+
+
+// //Post API to received Data from frontend
+// server.post("/",(req,res)=>{
+//     console.log("Received Data",req.body);
+
+//     const{username,city,age,pass} = req.body;
+
+//     //PassWord Check = if it is incorrect
+//     if(pass !== password){
+//         return res.status(400).json({message: "Password does not Match"})
+//     }
+
+//     //send Response back to frontend
+//     res.json({
+//         success:true,
+//         name : username,
+//         city,
+//         age,
+//         message:"This response is from Backend",
+//     });
+// });
+
+// server.listen(port,()=>{
+//     console.log(`Server is running on port ${port}`);
+// })
+
+
+
+
+// Frontend - middleware - Backend
 
 
 
