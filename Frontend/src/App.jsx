d353ce0bf9ec2081eca8fc1bbc94f318812af61b
namanydv <namanofficial91@gmail.com>
2026-01-30 09:09:53 +0530

    Revising COntext API

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,70 +1,84 @@
-import React, { useState } from "react";
-import axios from "axios"
-
-import "./App.css";
+import React from 'react'
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [city, setCity] = useState("");
-  const [age, setAge] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleInputs = async () => {
-    try{
-      const response = await axios.post("http://localhost:4444/",{
-        username: username,
-        city:city,
-        age:age,
-        pass: password,
-      });
-
-      console.log("Response:", response.data);
-      alert("Data Send Successfully")
-    }
-    catch (err) {
-      alert(err.response?.data?.message || err.message);
-  }
+  return (
+    <div style={{backgroundColor:"red"}}>
+      <h1>Context API</h1>
+    </div>
+  )
 }
 
-const GetData = async () => {
-  const response = await axios.get("http://localhost:4444/user");
-  console.log(response.data);
-};
+export default App
+
+
+
+// import React, { useState } from "react";
+// import axios from "axios"
+
+// import "./App.css";
+
+// const App = () => {
+//   const [username, setUsername] = useState("");
+//   const [city, setCity] = useState("");
+//   const [age, setAge] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleInputs = async () => {
+//     try{
+//       const response = await axios.post("http://localhost:4444/",{
+//         username: username,
+//         city:city,
+//         age:age,
+//         pass: password,
+//       });
+
+//       console.log("Response:", response.data);
+//       alert("Data Send Successfully")
+//     }
+//     catch (err) {
+//       alert(err.response?.data?.message || err.message);
+//   }
+// }
+
+// const GetData = async () => {
+//   const response = await axios.get("http://localhost:4444/user");
+//   console.log(response.data);
+// };
 
     
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="UserName"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="City"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Enter Your age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleInputs}>Send Data</button>
-      <button onClick={GetData}>Get Data from Server</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="UserName"
+//         value={username}
+//         onChange={(e) => setUsername(e.target.value)}
+//       />
+//       <input
+//         type="text"
+//         placeholder="City"
+//         value={city}
+//         onChange={(e) => setCity(e.target.value)}
+//       />
+//       <input
+//         type="text"
+//         placeholder="Enter Your age"
+//         value={age}
+//         onChange={(e) => setAge(e.target.value)}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Password"
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//       />
+//       <button onClick={handleInputs}>Send Data</button>
+//       <button onClick={GetData}>Get Data from Server</button>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
 
 // import React, { useState } from "react";
 // import axios from "axios";
