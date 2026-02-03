c05e1830eb3296c2122f82a1be9fa2bdcacc3a86
namanydv <namanofficial91@gmail.com>
2026-02-03 16:22:34 +0530

    Context Api in JavaScript

---
+++ Frontend/src/ClassComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Student from './Student'
+
+const ClassComponent = () => {
+  return (
+    <div style={{backgroundColor:"skyblue",padding:"10px"}}>
+        <h1>ClassComponent Component</h1>
+        <Student/>
+    </div>
+  )
+}
+
+export default ClassComponent
---
+++ Frontend/src/Student.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Subject from './Subject'
+
+const Student = () => {
+  return (
+    <div style={{backgroundColor:"lightgreen",padding:"10px"}}>
+        <h1>Student Component</h1>
+        <Subject/>
+    </div>
+  )
+}
+
+export default Student
