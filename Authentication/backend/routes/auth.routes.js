f8a86bb49e9da7f0560b06ab41e46caf2b5f3b0f
namanydv <namanofficial91@gmail.com>
2026-02-04 10:27:26 +0530

    creating a Logout Route

--- Authentication/backend/routes/auth.routes.js
+++ Authentication/backend/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import express from 'express'
-import { homepage, signup } from "../controllers/auth.controllers.js";
+import { homepage, login, logout, signup } from "../controllers/auth.controllers.js";
 
 const authRouter = express(Router())
 
 authRouter.post("/signup",signup)
 authRouter.get("/homepage",homepage)
+authRouter.post("/login",login)
+authRouter.post("/logout",logout)
 
 export default authRouter;
