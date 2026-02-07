49db2963c18618f447c4b272eb6400ba849b3b19
namanydv <namanofficial91@gmail.com>
2026-02-07 23:14:11 +0530

    .

--- Authentication/backend/routes/auth.routes.js
+++ Authentication/backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import express from 'express'
-import {homepage, login, logout, signup} from "../controllers/auth.controllers.js";
+import {createAccount, homepage, login, logout, signup} from "../controllers/auth.controllers.js";
 
 const authRouter = express(Router())
 
+authRouter.post("/createAccount",createAccount)
 authRouter.post("/signup",signup)
 authRouter.get("/homepage",homepage)
 authRouter.post("/login",login)
