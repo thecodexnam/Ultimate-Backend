4917da10f9ac8a408a701cba7722b4b8991eae13
namanydv <namanofficial91@gmail.com>
2026-02-10 18:11:37 +0530

    setup login page

---
+++ Authentication/backend/config/CLOUDINARY.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary } from 'cloudinary'
+import fs from "fs"
+import dotenv from 'dotenv'
+import { log } from 'console';
+
+    cloudinary.config({
+        cloud_name: process.env.CLOUINARY_CLOUD_NAME,
+        api_key: process.env.CLOUINARY_API_KEY,
+        api_secret:process.env.CLOUINARY_API_SECRET
+    });
+
+const uploadOnCloudinary = async (filepath)=>{
+    try {
+        if(!filepath){
+            return null
+        }
+        let result = cloudinary.uploader.upload(filepath)
+        console.log(result);
+        return result.secure_url
+        
+    } catch (error) {
+        fs.unlinkSync(filepath)
+        console.log(error);            
+    }
+}
+
+export default uploadOnCloudinary
