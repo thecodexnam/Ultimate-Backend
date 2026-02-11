648c55b76e0d15a030ee8109693db2fe456194f7
namanydv <namanofficial91@gmail.com>
2026-02-11 17:41:46 +0530

    feat: Add authentication routes, frontend user context, and install necessary dependencies.

--- Authentication/backend/routes/auth.routes.js
+++ Authentication/backend/routes/auth.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express"
 import express from 'express'
-import {homepage, login, logout, signup} from "../controllers/auth.controllers.js";
+import {getUserData, homepage, login, logout, signup} from "../controllers/auth.controllers.js";
+import { upload } from "../middleWares/multer.js";
+import { checkAuth } from "../middleWares/checkAuth.js";
 
 const authRouter = express(Router())
 
-authRouter.post("/signup",signup)
+
+authRouter.post("/signup",upload.single("profileImage"), signup)
 authRouter.get("/homepage",homepage)
 authRouter.post("/login",login)
 authRouter.post("/logout",logout)
+authRouter.get("/getuserdata",checkAuth,getUserData)
 
 export default authRouter;
--- Authentication/frontend/context/UserContext.jsx
+++ Authentication/frontend/context/UserContext.jsx
@@ -1,13 +1,27 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import App from '../src/App';
+import axios from 'axios';
 
 
 export const dataContext = createContext();
 const UserContext = ({children}) => {
+    let [userData,setUserData] = useState(null)
     const serverUrl = "http://localhost:8000"
+    const getUserData = async ()=>{
+        try {
+            let data = await axios.get(`${serverUrl}/api/getuserdata`)
+            
+        } catch (error) {
+            
+        }
+    }
+
+
     const value = {
         serverUrl
     }
+
+
   return (
     <div>
         <dataContext.Provider value={value}>
