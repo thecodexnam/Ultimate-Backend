1e2d578ffc005432318e85ba3ca55392c796d95e
namanydv <namanofficial91@gmail.com>
2026-02-12 11:03:26 +0530

    feat: Implement initial user authentication system with backend API and frontend UI.

--- Authentication/backend/routes/auth.routes.js
+++ Authentication/backend/routes/auth.routes.js
@@ -1,16 +1,15 @@
 import { Router } from "express"
-import express from 'express'
-import {getUserData, homepage, login, logout, signup} from "../controllers/auth.controllers.js";
+import { getUserData, homepage, login, logout, signup } from "../controllers/auth.controllers.js";
 import { upload } from "../middleWares/multer.js";
 import { checkAuth } from "../middleWares/checkAuth.js";
 
-const authRouter = express(Router())
+const authRouter = Router()
 
 
-authRouter.post("/signup",upload.single("profileImage"), signup)
-authRouter.get("/homepage",homepage)
-authRouter.post("/login",login)
-authRouter.post("/logout",logout)
-authRouter.get("/getuserdata",checkAuth,getUserData)
+authRouter.post("/signup", upload.single("profileImage"), signup)
+authRouter.get("/homepage", homepage)
+authRouter.post("/login", login)
+authRouter.post("/logout", logout)
+authRouter.get("/getuserdata", checkAuth, getUserData)
 
 export default authRouter;
