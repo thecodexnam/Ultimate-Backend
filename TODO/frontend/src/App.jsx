4b96e5629a0edd431de00ca9e199f9d6e15b81cf
namanydv <namanofficial91@gmail.com>
2026-02-28 16:10:41 +0530

    feat: add wrappy utility and update frontend components for task management

--- TODO/frontend/src/App.jsx
+++ TODO/frontend/src/App.jsx
@@ -3,6 +3,9 @@ import NavBar from './component/NavBar'
 import { Routes, Route } from 'react-router-dom'
 import AddTask from './component/AddTask'
 import TaskList from './component/TaskList'
+import UpdateTask from './component/UpdateTask'
+import Login from './component/Login'
+import Signup from './component/SignUp'
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
     <Routes>
       <Route path='/' element={<TaskList/>}/>
       <Route path='/add' element={<AddTask/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/update/:id' element={<UpdateTask/>}/>
     </Routes>
     </>
   )
--- TODO/frontend/src/component/AddTask.jsx
+++ TODO/frontend/src/component/AddTask.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import {data, useNavigate} from 'react-router-dom'
 import "../style/addtask.css";
 
 const AddTask = () => {
   const [taskData, setTaskData] = useState({
     title: "",
     description: "",
+    date:""
   });
+
+  const navigate = useNavigate();
 
   const handleAddTask = async (e) => {
     e.preventDefault();
@@ -23,6 +27,7 @@ const AddTask = () => {
 
       if (response.ok) {
         console.log("Task Added Successfully ✅", result);
+        navigate("/")
 
         // Reset form after success
         setTaskData({
@@ -39,8 +44,7 @@ const AddTask = () => {
 
   return (
     <div className="add-task-container">
-      <h1>Add New Task</h1>
-
+      <h1>Add New Task here</h1>
       <form className="add-task-form" onSubmit={handleAddTask}>
         <div className="form-group">
           <label>Task Title</label>
@@ -61,6 +65,17 @@ const AddTask = () => {
               setTaskData({ ...taskData, description: e.target.value })
             }
           ></textarea>
+        </div>
+
+        <div className="form-group">
+          <label>Select Date</label>
+          <input
+            type="date"
+            placeholder="Enter Task Title"
+            onChange={(e) =>
+              setTaskData({ ...taskData, date: e.target.value })
+            }
+          />
         </div>
 
         <button type="submit" className="submit-btn">
